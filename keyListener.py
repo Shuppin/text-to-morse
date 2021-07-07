4751b408b8c1194765d8a384d5ff793f8929878a
72602326 <etg3016@outlook.com>
2021-07-07 13:13:44 +0100

    Removed preset messages

---
+++ keyListener.py
@@ -0,0 +1,17 @@
+from pynput import keyboard
+import morse
+
+def on_press(key):
+    try:
+        morse.morse(key.char)
+    except AttributeError:
+        print('Ignoring special key {0}'.format(
+            key))
+
+with keyboard.Listener(
+        on_press=on_press) as listener:
+    listener.join()
+
+listener = keyboard.Listener(
+    on_press=on_press)
+listener.start()
